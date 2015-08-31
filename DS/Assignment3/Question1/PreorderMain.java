93a7aa2173a91c6b872ab0d7accf227dd504638a
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-08-31 10:07:29 +0530

    Update PreorderMain.java

--- DS/Assignment3/Question1/PreorderMain.java
+++ DS/Assignment3/Question1/PreorderMain.java
@@ -21,19 +21,19 @@ public class PreorderMain
 			switch (choice)
 			{
 				case 1 : System.out.println ("\nEnter item ");
-						 item = sc.nextInt();
-						 objPreorder.insertInTree ( item );
-						 break;
+					 item = sc.nextInt();
+					 objPreorder.insertInTree ( item );
+					 break;
 						 
 				case 2 : System.out.println("Tree in preorder: ");
-						 objPreorder.printPreorder();
-				 		 break;
+					 objPreorder.printPreorder();
+				 	 break;
 				
 				case 3 : System.exit(0);
-						 break;
+					 break;
 						 
 				default : System.out.println("Please enter correct choice");
-						  break;
+					  break;
 			}
 		}
 	}
