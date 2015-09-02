931154607ca147d95f14605c87565f5878a5de98
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-02 18:24:40 +0530

    Update BinarySearchFunctionMain.java

--- DS/Assignment7/BinarySearchFunctionMain.java
+++ DS/Assignment7/BinarySearchFunctionMain.java
@@ -21,28 +21,28 @@ public class BinarySearchFunctionMain
 			switch (choice)
 			{
 				case 1 : System.out.println("\nEnter size of array : ");
-						 int size = sc.nextInt();
-						 arr = new int [size];
-						 System.out.println("Enter items : ");
-						 for ( int index = 0; index < size; index++ )
-							 arr[index] = sc.nextInt();
-						 break;
+					 int size = sc.nextInt();
+					 arr = new int [size];
+					 System.out.println("Enter items : ");
+					 for ( int index = 0; index < size; index++ )
+						 arr[index] = sc.nextInt();
+					 break;
 						
 				case 2 : System.out.println("Enter item to search : ");
-						 int item = sc.nextInt();
-						 int left = 0, right = arr.length-1;
-						 int position = obj.binaryFunction ( arr, left, right, item ) ;
-						 if ( position == -1 )
-							 System.out.println ("Item does not exist in array");
-						 else
-							 System.out.println ("Leftmost position : " + obj.binaryFunction ( arr, left, right, item ) );
-						 break;
+					 int item = sc.nextInt();
+					 int left = 0, right = arr.length-1;
+					 int position = obj.binaryFunction ( arr, left, right, item ) ;
+					 if ( position == -1 )
+						 System.out.println ("Item does not exist in array");
+					 else
+						 System.out.println ("Leftmost position : " + obj.binaryFunction ( arr, left, right, item ) );
+					 break;
 						 
 				case 3 : System.exit(0);
-				 		 break;
+				 	 break;
 				
 				default : System.out.println("Please enter correct choice");
-				  		  break;
+				  	  break;
 			}
 		}
 	}
