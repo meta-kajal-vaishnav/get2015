b1812474bd8abc4348f34f1740e0d7864a295e2d
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-02 19:16:13 +0530

    Update BinarySearchFunctionMain.java

--- DS/Assignment7/BinarySearchFunctionMain.java
+++ DS/Assignment7/BinarySearchFunctionMain.java
@@ -7,43 +7,59 @@ public class BinarySearchFunctionMain
 		Scanner sc = null;
 		int choice, arr[] = null;
 		sc = new Scanner(System.in);
-		BinarySearchFunction obj = new BinarySearchFunction();
+		BinarySearchFunction objBinarySearchFunction = null;
 		
-		while (true)
-		{	
-			System.out.println("\n\nMenu : ");
-			System.out.println("1). Enter values in array");
-			System.out.println("2). Search item");
-			System.out.println("3). Exit");
-			System.out.println("\nEnter choice");
-			choice = sc.nextInt();
+		try
+		{
+			objBinarySearchFunction = new BinarySearchFunction();
 			
-			switch (choice)
-			{
-				case 1 : System.out.println("\nEnter size of array : ");
-					 int size = sc.nextInt();
-					 arr = new int [size];
-					 System.out.println("Enter items : ");
-					 for ( int index = 0; index < size; index++ )
-						 arr[index] = sc.nextInt();
-					 break;
-						
-				case 2 : System.out.println("Enter item to search : ");
-					 int item = sc.nextInt();
-					 int left = 0, right = arr.length-1;
-					 int position = obj.binaryFunction ( arr, left, right, item ) ;
-					 if ( position == -1 )
-						 System.out.println ("Item does not exist in array");
-					 else
-						 System.out.println ("Leftmost position : " + obj.binaryFunction ( arr, left, right, item ) );
-					 break;
-						 
-				case 3 : System.exit(0);
-				 	 break;
+			while (true)
+			{	
+				System.out.println("\n\nMenu : ");
+				System.out.println("1). Enter values in array");
+				System.out.println("2). Search item");
+				System.out.println("3). Exit");
+				System.out.println("\nEnter choice");
+				choice = sc.nextInt();
 				
-				default : System.out.println("Please enter correct choice");
-				  	  break;
+				switch (choice)
+				{
+					case 1 : System.out.println("\nEnter size of array : ");
+						 int size = sc.nextInt();
+						 arr = new int [size];
+						 System.out.println("Enter items : ");
+						 for ( int index = 0; index < size; index++ )
+							 arr[index] = sc.nextInt();
+						 break;
+							
+					case 2 : System.out.println("Enter item to search : ");
+						 int item = sc.nextInt();
+						 int left = 0, right = arr.length-1;
+						 int position = objBinarySearchFunction.binaryFunction ( arr, left, right, item ) ;
+						 if ( position == -1 )
+							 System.out.println ("Item does not exist in array");
+						 else
+							 System.out.println ("Leftmost position : " + objBinarySearchFunction.binaryFunction ( arr, left, right, item ) );
+						 break;
+							 
+					case 3 : System.exit(0);
+					 	 break;
+					
+					default : System.out.println("Please enter correct choice");
+					  	  break;
+				}
 			}
+		}
+		catch (Exception e)
+		{
+			System.out.println(e);
+		}
+		finally
+		{
+			if ( sc != null )
+				sc = null;
+			if ( objBinarySearchFunction != null )
+				objBinarySearchFunction = null;
 		}
 	}
 }
