d8b4c6f81f4131f5284c0bd4e47a8f5a35621e05
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-04 17:28:15 +0530

    Update BubbleSort.java

--- DS/Assignment8/Question2/BubbleSort.java
+++ DS/Assignment8/Question2/BubbleSort.java
@@ -1,7 +1,6 @@
 package Question2;
 
-public class BubbleSort 
-{
+public class BubbleSort {
 	int arr[];
 
 	public int[] getArr() {
@@ -11,22 +10,18 @@ public class BubbleSort
 	public void setArr(int[] arr) {
 		this.arr = arr;
 	}
-	
+
 	// bubble sort
-	public int[] bubbleSort ( )
-	{
+	public int[] bubbleSort() {
 		// outer loop for traversing
-		for ( int index = 0; index < arr.length-1 ; index++ ) 
-		{
+		for (int index = 0; index < arr.length - 1; index++) {
 			// inner loop for comparing
-			for ( int index1 = 0; index1 < arr.length-1-index ; index1++ )
-			{
+			for (int index1 = 0; index1 < arr.length - 1 - index; index1++) {
 				// if element is greater than next element swap them
-				if ( arr[index1] > arr[index1+1] )
-				{
+				if (arr[index1] > arr[index1 + 1]) {
 					int temp = arr[index1];
-					arr [index1] = arr [index1+1] ;
-					arr [index1+1] = temp;
+					arr[index1] = arr[index1 + 1];
+					arr[index1 + 1] = temp;
 				}
 			}
 		}
