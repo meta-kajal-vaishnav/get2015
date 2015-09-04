e9ab2e79ed69e2c82a884256937f4a7d95cc2db3
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-04 17:26:08 +0530

    Update RadixSort.java

--- DS/Assignment8/Question2/RadixSort.java
+++ DS/Assignment8/Question2/RadixSort.java
@@ -3,8 +3,7 @@ package Question2;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RadixSort 
-{
+public class RadixSort {
 	int arr[];
 
 	public int[] getArr() {
@@ -14,45 +13,42 @@ public class RadixSort
 	public void setArr(int[] arr) {
 		this.arr = arr;
 	}
-	
-	public int[] radixSort ( int largestValue )
-	{	
-		int maxBucketSize = 10;   // since buckets are from 0  to  9
-		
-		// create list of integer type of length maxBucketSize. Each element of this list is again a list itself
-		List<Integer> bucket[] = new ArrayList [maxBucketSize];
-		for ( int index = 0; index < bucket.length; index++ )
+
+	public int[] radixSort(int largestValue) {
+		int maxBucketSize = 10; // since buckets are from 0 to 9
+
+		// create list of integer type of length maxBucketSize. Each element of
+		// this list is again a list itself
+		List<Integer> bucket[] = new ArrayList[maxBucketSize];
+		for (int index = 0; index < bucket.length; index++)
 			bucket[index] = new ArrayList<Integer>();
-		
+
 		int count = 1;
-		
-		// loop runs till no of digits in largest number 
-		while ( largestValue/count > 0 )
-		{
-			// takes digit at unit's place when while loop is run first time. Second time it takes digit at tenth place & so on.
-			for ( int index = 0; index < arr.length; index++ )
-			{
-				int unitsDigit = ( arr[index] / count ) % 10; 
-				bucket[unitsDigit].add ( arr[index] );
+
+		// loop runs till no of digits in largest number
+		while (largestValue / count > 0) {
+			/* takes digit at unit's place when while loop is run first time. 
+			  Second time it takes digit at tenth place & so on. */
+			for (int index = 0; index < arr.length; index++) {
+				int unitsDigit = (arr[index] / count) % 10;
+				bucket[unitsDigit].add(arr[index]);
 			}
-			
+
 			int index = -1;
-			
+
 			// take all digits from lists in FIFO manner & place them again in array for further processing
-			for ( List<Integer> item : bucket )
-			{
-				for ( int item1 : item )
-				{
+			for (List<Integer> item : bucket) {
+				for (int item1 : item) {
 					index++;
 					arr[index] = item1;
 				}
 				// clear each list.
 				item.clear();
 			}
-			
-			count *= 10;	// update count
+
+			count *= 10; // update count
 		}
-		
-		return arr;   // return sorted array
+
+		return arr; // return sorted array
 	}
 }
