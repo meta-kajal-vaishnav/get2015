be9c264df2dac6a778e1eed44786eb5038943088
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-04 17:27:16 +0530

    Update QuickSort.java

--- DS/Assignment8/Question2/QuickSort.java
+++ DS/Assignment8/Question2/QuickSort.java
@@ -1,7 +1,6 @@
 package Question2;
 
-public class QuickSort 
-{
+public class QuickSort {
 	int arr[];
 
 	public int[] getArr() {
@@ -11,40 +10,38 @@ public class QuickSort
 	public void setArr(int[] arr) {
 		this.arr = arr;
 	}
-	
-	public int[] quickSort ( int pivot, int end )  // initially pivot is = first element of array and end = arr.length
-	{
-		int storeIndex = pivot + 1;   // storeIndex contains pivot's next element
-		
+
+	// initially pivot is = first element of array and end = arr.length
+	public int[] quickSort(int pivot, int end) {
+		// storeIndex contains pivot's next element
+		int storeIndex = pivot + 1;
+
 		// traverse array for sorting
-		for ( int index = pivot + 1; index < end; index++ )
-		{
-			if ( arr[index] < arr[pivot] )
-			{
+		for (int index = pivot + 1; index < end; index++) {
+			if (arr[index] < arr[pivot]) {
 				// swap index and storeIndex
 				int temp = arr[index];
-				arr [index] = arr [storeIndex] ;
-				arr [storeIndex] = temp;
-				// increment storeIndex 
+				arr[index] = arr[storeIndex];
+				arr[storeIndex] = temp;
+				// increment storeIndex
 				storeIndex++;
 			}
 		}
-		
+
 		// swap pivot and storeIndex-1
 		int temp = arr[pivot];
-		arr [pivot] = arr [storeIndex-1] ;
-		arr [storeIndex-1] = temp;
-		
+		arr[pivot] = arr[storeIndex - 1];
+		arr[storeIndex - 1] = temp;
+
 		// element has reached its correct position so increment pivot by one
-		if ( pivot == storeIndex-1 )
-		{	
-			pivot = pivot+1;
+		if (pivot == storeIndex - 1) {
+			pivot = pivot + 1;
 			// pivot has reached end of array
-			if ( pivot == end-1 )
-				return arr;  // return sorted array
+			if (pivot == end - 1)
+				return arr; // return sorted array
 		}
-		quickSort ( pivot, end );
+		quickSort(pivot, end);
 		return arr;
 	}
-	
+
 }
