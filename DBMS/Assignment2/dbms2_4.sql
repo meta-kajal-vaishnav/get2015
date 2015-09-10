4b486e813b434baf08e907a8e9d673cedc992a3b
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-10 18:33:34 +0530

    Update dbms2_4.sql

--- DBMS/Assignment2/dbms2_4.sql
+++ DBMS/Assignment2/dbms2_4.sql
@@ -1,63 +1,63 @@
 -- Creating Database zipcode
 CREATE DATABASE zipcode;
+
+-- Use Database zipcode
 USE zipcode;
 
--- creating table Zipcode
- CREATE TABLE zipcodeInfo
+-- Creating table stateInfo
+CREATE TABLE stateInfo
 (
-	zipcode_id int PRIMARY KEY AUTO_INCREMENT,
-  	zipcode_number int NOT NULL
+	-- to store state id and state Name
+	state_id int PRIMARY KEY AUTO_INCREMENT,
+	state_name varchar(20)
 );
 
 -- Creating table cityInfo
 CREATE TABLE cityInfo
 (
-	-- To store zip id and city name
+	-- To store state id and city name
 	city_id int PRIMARY KEY AUTO_INCREMENT,
 	city_name varchar(20),
-	 zipcode_id int,
-	FOREIGN KEY (zipcode_id) REFERENCES zipcodeInfo (zipcode_id) 
+	state_id int,
+	FOREIGN KEY (state_id) REFERENCES stateInfo (state_id) 
 );
 
--- Creating table stateInfo
-CREATE TABLE stateInfo
+-- creating table Zipcode
+ CREATE TABLE zipcodeInfo
 (
-	-- to store zipid and state Name
-	state_id int PRIMARY KEY AUTO_INCREMENT,
-	state_name varchar(20),
-  	city_id int,
-	FOREIGN KEY (city_id) REFERENCES cityInfo (city_id) 
+	zipcode_number int PRIMARY KEY AUTO_INCREMENT,
+  	city_id int NOT NULL,
+  	FOREIGN KEY (city_id) REFERENCES cityInfo (city_id)
 );
- 
- -- Inserting values in zipcodeInfo table
- INSERT INTO zipcodeInfo (zipcode_id, zipcode_number) VALUES (1, 5001);
- INSERT INTO zipcodeInfo (zipcode_number) VALUES (5002);
- INSERT INTO zipcodeInfo (zipcode_number) VALUES (5003);
- INSERT INTO zipcodeInfo (zipcode_number) VALUES (5004);
- 
- -- Inserting values in cityInfo table
- INSERT INTO cityInfo (city_id, city_name, zipcode_id) VALUES (1,'Ahmedabad',1);
- INSERT INTO cityInfo (city_name, zipcode_id) VALUES ('Udaipur',2);
- INSERT INTO cityInfo (city_name, zipcode_id) VALUES ('Delhi',3);
- INSERT INTO cityInfo (city_name, zipcode_id) VALUES ('Kota',2);
- INSERT INTO cityInfo (city_name, zipcode_id) VALUES ('Kolkata',4);
- 
+
+
  -- Inserting values in stateInfo table
- INSERT INTO stateInfo (state_id, state_name, city_id) VALUES (1,'Gujarat',1);
- INSERT INTO stateInfo (state_name, city_id) VALUES ('Rajasthan',2);
- INSERT INTO stateInfo (state_name, city_id) VALUES ('Delhi',3);
- INSERT INTO stateInfo (state_name, city_id) VALUES ('Rajasthan',4);
- INSERT INTO stateInfo (state_name, city_id) VALUES ('West Bengal',5);
- 
- 
- -- Query to find the zip code, city name and stateName
+ INSERT INTO stateInfo (state_id, state_name) VALUES (1,'Gujarat');
+ INSERT INTO stateInfo (state_name) VALUES ('Rajasthan');
+ INSERT INTO stateInfo (state_name) VALUES ('Delhi');
+ INSERT INTO stateInfo (state_name) VALUES ('West Bengal');
+
+-- Inserting values in cityInfo table
+ INSERT INTO cityInfo (city_id, city_name, state_id) VALUES (1,'Ahmedabad',1);
+ INSERT INTO cityInfo (city_name, state_id) VALUES ('Udaipur',2);
+ INSERT INTO cityInfo (city_name, state_id) VALUES ('Delhi',3);
+ INSERT INTO cityInfo (city_name, state_id) VALUES ('Kota',2);
+ INSERT INTO cityInfo (city_name, state_id) VALUES ('Kolkata',4);
+
+-- Inserting values in zipcodeInfo table
+ INSERT INTO zipcodeInfo (zipcode_number, city_id) VALUES (1, 1);
+ INSERT INTO zipcodeInfo (city_id) VALUES (2);
+ INSERT INTO zipcodeInfo (city_id) VALUES (3);
+ INSERT INTO zipcodeInfo (city_id) VALUES (4);
+ INSERT INTO zipcodeInfo (city_id) VALUES (5);
+
+
+  -- Query to find the zip code, city name and stateName
  SELECT z.zipcode_number, c.city_name, s.state_name 
  from zipcodeInfo z
- INNER JOIN cityInfo c ON z.zipcode_id = c.zipcode_id
- INNER JOIN stateInfo s ON c.city_id = s.city_id
- WHERE z.zipcode_number = 5002
+ INNER JOIN cityInfo c ON z.city_id = c.city_id
+ INNER JOIN stateInfo s ON c.state_id = s.state_id
  ORDER BY state_name, city_name;
-
  
  
  
