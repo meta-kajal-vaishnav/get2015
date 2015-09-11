470f7867a40191afb93c7b8cc823a1d7f30b1929
meta-kajal-vaishnav <kajal.vaishnav@metacube.com>
2015-09-11 10:14:07 +0530

    Update dbms2_2.sql

--- DBMS/Assignment2/dbms2_2.sql
+++ DBMS/Assignment2/dbms2_2.sql
@@ -3,5 +3,10 @@ USE library_information_system;
 
 -- Question2
 
-SELECT b.accession_number AS Acc_no ,b.member_id AS Mem_id, b.issue_date AS Iss_d, b.return_date AS Ret_d 
-FROM Book_return b INNER JOIN Members m  ON m.member_id = b.member_id ORDER BY issue_date , m.member_name;
+SELECT bi.accession_number AS Acc_no , bi.member_id AS Mem_id, bi.issue_date AS Iss_d, IFNULL(br.return_date, " ") AS Ret_d 
+FROM Book_return br
+RIGHT JOIN Book_issue bi 
+ON br.member_id = bi.member_id 
+INNER JOIN Members m  
+ON m.member_id = bi.member_id 
+ORDER BY bi.issue_date , m.member_name;
